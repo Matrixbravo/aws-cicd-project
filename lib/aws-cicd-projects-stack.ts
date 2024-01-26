8f796e466b86ca41645424dba66513e06ac9554f
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2024-01-26 14:40:58 +0530

    Added visitor counter

--- lib/aws-cicd-projects-stack.ts
+++ lib/aws-cicd-projects-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as dotenv from "dotenv";
 
 export class AwsCicdProjectsStack extends cdk.Stack {
@@ -11,14 +12,26 @@ export class AwsCicdProjectsStack extends cdk.Stack {
     //Load the environment .env file.
     dotenv.config();
 
+    // Create a table to store some data.
+    const table = new dynamodb.Table(this, "VisitorTimeTable", {
+      partitionKey: {
+        name: "key",
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    })
+
     const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("lambda"),
       handler: "main.handler",
       environment: {
-        VERSION: process.env.VERSION || "0.0"
+        VERSION: process.env.VERSION || "0.0",
+        TABLE_NAME: table.tableName
       }
     });
+
+    table.grantReadWriteData(lambdaFunction);
 
 
     const functionUrl = lambdaFunction.addFunctionUrl({
