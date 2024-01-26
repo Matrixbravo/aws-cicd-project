dd5b159c0f3e85bccde23f7342b39008ee552886
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2024-01-26 12:12:20 +0530

    Updated Python code

--- lambda/main.py
+++ lambda/main.py
@@ -3,7 +3,7 @@
 
 def handler(event, context):
     response_body = {
-        "message": "Hello World",
+        "message": "Hello Worlds",
         "version": "1.0.0"
     }
     return {"statusCode": 200, "body": response_body}
