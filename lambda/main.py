ce5e42040b6786e3bc26b123c8162066c3d33e1f
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2024-01-26 14:28:45 +0530

    Added once cache node.js dependencies

--- lambda/main.py
+++ lambda/main.py
@@ -3,7 +3,7 @@ import os
 def handler(event, context):
     version = os.environ.get("VERSION", "0.0")
     response_body = {
-        "message": "Hello test",
+        "message": "Hello world",
         "version": version
     }
     return {"statusCode": 200, "body": response_body}
