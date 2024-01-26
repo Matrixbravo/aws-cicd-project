a149acaacdb18477c9e4dd2115eee33b4147e077
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2024-01-26 11:17:31 +0530

    Added Workflow

--- lambda/main.py
+++ lambda/main.py
@@ -2,8 +2,8 @@
 
 
 def handler(event, context):
-    print(event)
-    return {
-        "statusCode": 200,
-        "body": "Hello World"
+    response_body = {
+        "message": "Hello World",
+        "version": "1.0.0"
     }
+    return {"statusCode": 200, "body": response_body}
