54f60da97ee27a26419f55a9ccc433af3985167f
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2024-01-26 10:54:26 +0530

    Initial commit

---
+++ lambda/main.py
@@ -0,0 +1,9 @@
+
+
+
+def handler(event, context):
+    print(event)
+    return {
+        "statusCode": 200,
+        "body": "Hello World"
+    }
--- lib/aws-cicd-projects-stack.ts
+++ lib/aws-cicd-projects-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda"
 
 export class AwsCicdProjectsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "main.handler"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCicdProjectsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const functionUrl = lambdaFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors:{
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"]
+      }
+    });
+
+    new cdk.CfnOutput(this, "Url", {
+      value: functionUrl.url,
+    });
   }
 }
