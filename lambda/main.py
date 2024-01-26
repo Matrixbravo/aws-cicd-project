ede01a4df8d1b6aa2af66e7590a4023d0dd9a995
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2024-01-26 14:08:17 +0530

    Added .env file

--- lambda/main.py
+++ lambda/main.py
@@ -3,7 +3,7 @@ import os
 def handler(event, context):
     version = os.environ.get("VERSION", "0.0")
     response_body = {
-        "message": "Hello Worlds",
+        "message": "Hello World",
         "version": version
     }
     return {"statusCode": 200, "body": response_body}
